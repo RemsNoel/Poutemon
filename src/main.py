971c7aab341de83b7e0baf3c64cb420b6bc1b419
Tintou <martin.lm@outlook.fr>
2020-01-09 11:13:47 +0100

    Restructuration du code + changement de texte possible

--- src/main.py
+++ src/main.py
@@ -6,19 +6,29 @@ def main():
      
     # initialize the pygame module
     pygame.init()
+
+    # colors
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    GREEN = (0, 255, 0) # Pas très joli mais c'est pas grave les amis
+    BLUE = (0, 0, 128) # Celui non plus mais on fait du mieux qu'on peut
+
+    # create a surface on screen that has the size of 1280 x 720
+    screen_width = 750;
+    screen_height = 596;
+    
+    screen = pygame.display.set_mode((screen_width,screen_height))
+    screen_rect = screen.get_rect()
+
+    
     # load and set the logo
     logo = pygame.image.load("droleabeille.png")
     pygame.display.set_icon(logo)
     pygame.display.set_caption("Poutemon")
     
+    # cursor
     pygame.mouse.set_cursor(*pygame.cursors.tri_right)
      
-
-    # create a surface on screen that has the size of 1280 x 720
-    screen_width = 750;
-    screen_height = 596;
-
-    screen = pygame.display.set_mode((screen_width,screen_height))
 
     #Bg image
     bg1 = pygame.image.load("bg1.png")
@@ -27,50 +37,81 @@ def main():
      
     # define a variable to control the main loop
     running = True
-     
+
+    done = False
+    clock = pygame.time.Clock()
+
+    # class for the text part
+    def make_text(message, size, color):
+        font = pygame.font.SysFont('Arial', size)
+        text = font.render(message,True,color)
+        rect = text.get_rect(center = (screen_rect.centerx, screen_rect.centery) )
+        return text,rect
+    
+    pygame_text = []
+    possible_outcome = ['Je vais te goumer.', 'Tva voir.', 'rira bien qui rira le dernier.']
+    for outcome in possible_outcome:
+        pygame_text.append(make_text(outcome, 24, BLUE))
+    text,rect = pygame_text[0]#set a default
+
+    #Display an image
+    image = pygame.image.load("angry.png")
+    image1 = pygame.image.load("chut.png")
+    dialogue = pygame.image.load("dialogue.png")
+
+    
+
     # main loop
-    while running:
+    while not done:
         # event handling, gets all event from the event queue
         for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    text,rect = pygame_text[0]
+                elif event.key == pygame.K_b:
+                    text,rect = pygame_text[1]
+                elif event.key == pygame.K_c:
+                    text,rect = pygame_text[2]
+                elif event.key == pygame.K_ESCAPE:
+                    done = True
 
-            white = (255, 255, 255) 
-            green = (0, 255, 0) 
-            blue = (0, 0, 128) 
-
-            font = pygame.font.Font('freesansbold.ttf', 24) 
-            text = font.render('Je...Je vais te goumer.', True, blue)
-
-            #Display an image
-            image = pygame.image.load("angry.png")
-            image1 = pygame.image.load("chut.png")
-            dialogue = pygame.image.load("dialogue.png")
-
-            # define the position of the smiley
+            # # define the position of the smiley
             xpos = 29
             ypos = 50
 
-            # now blit the smiley on screen
+            # # now blit the smiley on screen
+            # screen.blit(image1, (-15, -250))
+            # screen.blit(image, (xpos, ypos))
+            # screen.blit(dialogue, (-102, 410))
+            # screen.blit(text, (40, 486))
+            # # and update the screen (don't forget that!)
+            # pygame.display.flip()
+
+            # if event.type == pygame.MOUSEBUTTONDOWN:
+            #     if event.button == 1 and event.pos[0] < screen_width and event.pos[1] < screen_height:
+            #         text2 =  font.render('Ahah... jrigole', True, BLUE)
+            #         screen.blit(dialogue, (-102, 410))
+            #         screen.blit(text2, (40, 486))
+            #         pygame.display.flip()
+
             screen.blit(image1, (-15, -250))
             screen.blit(image, (xpos, ypos))
             screen.blit(dialogue, (-102, 410))
-            screen.blit(text, (40, 486))
-            # and update the screen (don't forget that!)
+            screen.blit(text, [40, 486])
             pygame.display.flip()
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1 and event.pos[0] < screen_width and event.pos[1] < screen_height:
-                    text2 =  font.render('Ahah... jrigole', True, blue)
-                    screen.blit(dialogue, (-102, 410))
-                    screen.blit(text2, (40, 486))
-                    pygame.display.flip()
+        
+            clock.tick(60)
 
 
 
 
-            # only do something if the event is of type QUIT
-            if event.type == pygame.QUIT:
-                # change the value to False, to exit the main loop
-                running = False
+            # # only do something if the event is of type QUIT
+            # if event.type == pygame.QUIT:
+            #     # change the value to False, to exit the main loop
+            #     running = False
+    pygame.quit()
      
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)

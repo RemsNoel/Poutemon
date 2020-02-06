2ed286a279bb88df961e521fc1d945a5a1ab2645
Tintou <martin.lm@outlook.fr>
2020-02-06 19:00:28 +0100

    Image boutique (test)

--- src/boutique.py
+++ src/boutique.py
@@ -6,9 +6,11 @@ from avance import *
 class boutique():
 
     def __init__(self, screen, Avancement):
-        self.fond = pygame.image.load("./resources/fond_ecran/test.png")
+        self.fond = pygame.image.load("./resources/boutique/shop_pokemon.jpg")
         self.fond = pygame.transform.scale(self.fond, (1500, 1000))
+        self.pokeball = pygame.image.load("./resources/boutique/pokeball.png")
         self.etat = True
+
         self.avancement = Avancement
         self.screen = screen
 
@@ -17,5 +19,12 @@ class boutique():
 
     def blitbase(self):
         self.screen.blit(self.fond, (0, 0))
+        self.screen.blit(self.pokeball, (45, 200))
         pygame.display.flip()
+
+    def quitter(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+
 
